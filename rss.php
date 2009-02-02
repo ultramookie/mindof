e4de83eccff8d41e6c4aff989d66e85ebefd2743
Mookie Kong <mookie@ymail.com>
2009-02-01 23:56:21 -0800

    fixed up rss feed with recommendations from feedvalidator.org.

--- rss.php
+++ rss.php
@@ -1,4 +1,5 @@
 <?php
+header('Content-type: application/rss+xml');
 
 include_once("db.php");
 include_once("mindoflib.php");
@@ -9,12 +10,16 @@ echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
 
 ?>
 
-<rss version="2.0">
+<rss version="2.0"
+	xmlns:content="http://purl.org/rss/1.0/modules/content/"
+	xmlns:atom="http://www.w3.org/2005/Atom"
+>
   <channel>
 	<title><?php echo $sitename; ?></title>
 	<link><?php echo $siteurl; ?></link>
 	<description><?php echo $sitename; ?></description>
 	<generator>mindof <?php echo $version; ?></generator>
+	<atom:link href="<?php echo $siteurl; ?>/rss.php" rel="self" type="application/rss+xml" />
 	<ttl>5</ttl>
 <?php
 	printRSS($numRss);
