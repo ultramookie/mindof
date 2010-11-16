db4e8a4847db28610a8017f052765120e616b772
Mookie Kong <mookie@ymail.com>
2010-11-15 21:03:34 -0800

    minor update of url

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <hr />
 <p class="footer">
-powered by <a href="http://mindof-project.org">mindof</a> written by steve "<a href="http://ultramookie.com">mookie</a>" kong.<br />
+powered by <a href="http://mindof-project.org">mindof</a> written by steve "<a href="http://mookiesplace.com">mookie</a>" kong.<br />
 <a href="http://mindof-project.org">mindof</a> is distributed under <a href="http://www.gnu.org/licenses/gpl-3.0.html">gplv3</a>.<br />
 </p>
 </div>
